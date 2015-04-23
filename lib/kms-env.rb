254046d034d919d75b9da2e29f287bad2c6290c8
Drew Stokes <drew@drewstokes.com>
2015-04-23 10:11:48 -0700

    use aws region specified in env

--- lib/kms-env.rb
+++ lib/kms-env.rb
@@ -12,35 +12,35 @@ module KmsEnv
   ###
   # Load decrypted environment variables
   ###
-  
+
   def kms
-    @kms ||= Aws::KMS::Client.new(region: 'us-east-1')
+    @kms ||= Aws::KMS::Client.new(region: ENV['AWS_REGION'] || 'us-east-1')
   end
-  
+
   def ciphertext_blob_for(text)
     Base64.decode64(text)
   end
-  
-  def kms_decrypt_text(text)
-    kms.decrypt(ciphertext_blob: text)
+
+  def kms_decrypt_blob(blob)
+    kms.decrypt(ciphertext_blob: blob)
   rescue Exception => e
     self.logger.error("Failed to decrypt #{key} with error #{e.class}")
   end
-  
+
   def plaintext_key_for(key)
     key.gsub(kms_key_matcher, '')
   end
-  
+
   def set_decrypted_env_for(key)
     # skip values that have already been decrypted
     return if ENV[plaintext_key_for(key)]
-    ENV[plaintext_key_for(key)] = kms_decrypt_text(ciphertext_blob_for(ENV[key])).first.plaintext    
+    ENV[plaintext_key_for(key)] = kms_decrypt_blob(ciphertext_blob_for(ENV[key])).first.plaintext
   end
-  
+
   def kms_key_matcher
     /_KMS$/
   end
-  
+
   def load
     ENV.keys.select {|k| k =~ kms_key_matcher}.each do |key|
       set_decrypted_env_for(key)
