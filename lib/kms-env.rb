b4dd8f30f19056fbd2dc9b46e0993f79cbe047f0
Aaron Harpole <aharpole@gmail.com>
2015-04-23 01:11:26 -0400

    refactored the hell out of this

--- lib/kms-env.rb
+++ lib/kms-env.rb
@@ -12,26 +12,38 @@ module KmsEnv
   ###
   # Load decrypted environment variables
   ###
+  
+  def kms
+    @kms ||= Aws::KMS::Client.new(region: 'us-east-1')
+  end
+  
+  def ciphertext_blob_for(text)
+    Base64.decode64(text)
+  end
+  
+  def kms_decrypt_text(text)
+    kms.decrypt(ciphertext_blob: text)
+  rescue Exception => e
+    self.logger.error("Failed to decrypt #{key} with error #{e.class}")
+  end
+  
+  def plaintext_key_for(key)
+    key.gsub(matcher, '')
+  end
+  
+  def set_decrypted_env_for(key)
+    # skip values that have already been decrypted
+    return if ENV[plaintext_key_for(key)]
+    ENV[plaintext_key_for(key)] = kms_decrypt_text(ciphertext_blob_for(ENV[key])).first.plaintext    
+  end
+  
+  def matcher
+    /_KMS$/
+  end
+  
   def load
-    matcher = /_KMS$/
-    kms = Aws::KMS::Client.new(region: 'us-east-1')
-
-    ENV.keys.each do |key|
-      # skip keys not ending in _KMS
-      next unless key =~ matcher
-
-      # skip values that have already been decrypted
-      plaintext_key = key.gsub(matcher, '')
-      next if ENV[plaintext_key]
-
-      ciphertext_blob = Base64.decode64(ENV[key])
-      begin
-        # binding.pry
-        resp = kms.decrypt(ciphertext_blob: ciphertext_blob)
-        ENV[plaintext_key] = resp.first.plaintext
-      rescue Exception => e
-        self.logger.error("Failed to decrypt #{key} with error #{e.class}")
-      end
+    ENV.keys.select {|k| k =~ matcher}.each do |key|
+      set_decrypted_env_for(key)
     end
   end
 
